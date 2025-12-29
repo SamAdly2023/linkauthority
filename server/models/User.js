19b02089d19880404950a0979663f65144b38cac
Sam Adly <samadly728@gmail.com>
2025-12-30 03:08:16 +0800

    Relax User model validation to prevent login errors

--- server/models/User.js
+++ server/models/User.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
   googleId: { type: String, unique: true },
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  name: { type: String },
+  email: { type: String },
   points: { type: Number, default: 100 },
   websites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Website' }],
   transactions: [{
